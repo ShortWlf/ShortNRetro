5f3f03761936a900495099c7e59793d80cb18b1f
Sludge Entertainment <13711953+ShortWlf@users.noreply.github.com>
2026-02-11 03:54:44 -0600

    Update chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -47,7 +47,8 @@ document.querySelectorAll("#room-list li").forEach(li => {
 // ------------------------------
 
 function buildIRCUrl(room) {
-    return `https://web.libera.chat/#/?join=%23aghq_${room}&theme=dark`;
+    // This is the ONLY format that makes the channel field clean
+    return `https://web.libera.chat/#aghq_${room}`;
 }
 
 // ------------------------------
